edbd12ab70e11530aedcd5fff25f5ade69090c4e
Dominik Finkbeiner <finkes93@gmail.com>
2016-03-30 21:35:48 +0200

    add express, add some grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,23 @@ module.exports = function(grunt) {
   grunt.initConfig({
     ts: {
       default : {
-        src: ["**/*.ts", "!node_modules/**/*.ts"]
+        src: ["**/*.ts", "!node_modules/**/*.ts"],
+        options: {
+            module: "commonjs",
+            moduleResolution: "node"
+        }
       }
+    },
+    execute: {
+        default: {
+            src: ['index.js']
+        }
     }
   });
   grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-execute');
+  
   grunt.registerTask("default", ["ts"]);
+  grunt.registerTask("compile", ["ts"]);
+  grunt.registerTask("run", ["ts", "execute"])
 };
--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-function greeter(person) {
-    return "Hello, " + person;
+declare function require(name:string);
+
+var express = require("express");
+var app = express();
+
+function helloWorld(request, response){
+    response.send("Hello World");
 }
 
-var user = "Jane User";
+app.get('/', helloWorld);
 
-console.log(greeter(user));
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
